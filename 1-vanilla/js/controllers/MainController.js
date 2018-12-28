648024c780d7be209904a9f07066efdd24d7db42
smallbee <smallbee3@gmail.com>
2018-12-28 23:19:55 +0900

    vanillajs| ResultView 1 - Rendering search result at '#search-result' of index.html, which is triggered by 'this.search(input)' at onSubmit method

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,7 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+
+import SearchModel from '../models/SearchModel.js'
 
 const tag = '[MainController]'
 
@@ -8,12 +11,31 @@ export default {
     FormView.setup(document.querySelector('form'))
       .on('@submit', e => this.onSubmit(e.detail.input))  // e.detail에서 input 속성으로 FormView.js에서 parameter로 전달한 값이 넘어올거예요.
       .on('@reset', e => this.onResetForm())
+
+    ResultView.setup(document.querySelector('#search-result'))
   },
 
   onSubmit(input) {
     console.log(tag, 'onSubmit()', input)
+    this.search(input)
   },
+
   onResetForm() {
     console.log(tag, 'onResetForm()')
+  },
+
+  search(query) {
+    console.log(tag, 'search()', query)
+    // search api
+    // 1) no value
+    // this.onSearchresult([])
+    // 2) data from SearchModel.js (dummy data)
+    SearchModel.list(query).then(data => {
+      this.onSearchresult(data)
+    })
+  },
+
+  onSearchresult(data) {
+    ResultView.render(data)
   }
 }
