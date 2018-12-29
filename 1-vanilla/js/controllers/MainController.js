7683f8a941d9ee5d2cca92da9d311419b43a9d70
smallbee <smallbee3@gmail.com>
2018-12-30 00:50:57 +0900

    vanillajs| KeywordView 1 - KeywordView.js added and rendered by fetchSearchKeyword method of renderView

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,8 +1,10 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
+import KeywordView from '../views/KeywordView.js'
 
 import SearchModel from '../models/SearchModel.js'
+import KeywordModel from '../models/KeywordModel.js'
 
 const tag = '[MainController]'
 
@@ -16,6 +18,8 @@ export default {
     TabView.setup(document.querySelector('#tabs'))
       .on('@change', e => this.onChangeTab(e.detail.tabName))
 
+    KeywordView.setup(document.querySelector('#search-keyword'))
+
     ResultView.setup(document.querySelector('#search-result'))
 
     this.selectedTab = '추천 검색어'
@@ -25,7 +29,23 @@ export default {
   renderView() {
     console.log(tag, 'renderView')
     TabView.setActiveTab(this.selectedTab)
+
+    if (this.selectedTab === '추천 검색어') {
+      // KeywordModel.list().then(data => {
+      //   KeywordView.render(data)
+      // })
+      // View를 rendering하는 곳이기 때문에 KeywordModel.js에서 data를 가져오는 코드를 아래 fetchSearchKeyword()로 분리
+      this.fetchSearchKeyword()
+    } else {
+
+    }
     ResultView.hide()
+  },
+
+  fetchSearchKeyword() {
+    KeywordModel.list().then(data => {
+      KeywordView.render(data)
+    })
   },
 
   onSubmit(input) {
