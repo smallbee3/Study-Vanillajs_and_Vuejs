4d4fb3dad284e0f3625c293e2dae4b4739482710
smallbee <smallbee3@gmail.com>
2018-12-31 12:41:31 +0900

    vuejs| Form2 - Request 4 done by using v-on:keyup="onKeyup" and v-on:click="onReset"

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -7,6 +7,17 @@ new Vue({
     onSubmit(e) {
       // e.preventDefault() // -> vanilla.js 방식
       // index.html에서 v-on:submit.prevent="onSubmit" -> vue.js 방식
+    },
+    onKeyup(e) {
+      if (!this.query.length) this.resetForm()
+    },
+    onReset(e) {
+      this.resetForm()
+    },
+    resetForm() {
+      this.query = ''
+      // todo remove results ..
+      debugger
     }
   }
 })
