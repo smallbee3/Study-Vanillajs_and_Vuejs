b7fb994384159b05fafda17f53c017d08e2b82a6
smallbee <smallbee3@gmail.com>
2018-12-31 13:47:54 +0900

    vuejs| Result2 - Request 3 done by changing the value of data attributes 'submitted' and 'searchResult'

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -27,8 +27,8 @@ new Vue({
     },
     resetForm() {
       this.query = ''
-      // todo remove results ..
-      debugger
+      this.submitted = false
+      this.searchResult = []
     }
   }
 })
