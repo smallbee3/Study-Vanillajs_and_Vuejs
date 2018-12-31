60d3f95f78dc7500537f66f2d734af3c0c20035e
smallbee <smallbee3@gmail.com>
2018-12-31 11:51:24 +0900

    vuejs| Form1 - Request 1,2,3 done by using v-model, v-show, v-on:submit.prevent

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    msg: 'hello world'
+    query: ''
+  },
+  methods: {
+    onSubmit(e) {
+      // e.preventDefault() // -> vanilla.js 방식
+      // index.html에서 v-on:submit.prevent="onSubmit" -> vue.js 방식
+    }
   }
 })
