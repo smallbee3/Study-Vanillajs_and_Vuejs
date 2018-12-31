ebf56d02871f05060c8cb3ebcf25869fef05501f
smallbee <smallbee3@gmail.com>
2018-12-31 13:13:54 +0900

    vuejs| Result1 - Request 1,2 done by 'v-if', 'v-else' and 'v-bind' directives

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,18 +1,29 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
   el: '#app',
   data: {
-    query: ''
+    query: '',
+    submitted: false,
+    searchResult: []
   },
   methods: {
     onSubmit(e) {
       // e.preventDefault() // -> vanilla.js 방식
       // index.html에서 v-on:submit.prevent="onSubmit" -> vue.js 방식
+      this.search()
     },
     onKeyup(e) {
       if (!this.query.length) this.resetForm()
     },
     onReset(e) {
       this.resetForm()
+    },
+    search() {
+      SearchModel.list().then(data => {
+        this.submitted = true
+        this.searchResult = data
+      })
     },
     resetForm() {
       this.query = ''
