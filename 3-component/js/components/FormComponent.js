e11a932df749fa55474a54602c38d18498a3dae4
smallbee <smallbee3@gmail.com>
2019-01-02 23:41:34 +0900

    component| TabComponent 2 - emit tab change event to parent component and pass tabs and slectedTab data to child component

--- 3-component/js/components/FormComponent.js
+++ 3-component/js/components/FormComponent.js
@@ -4,6 +4,19 @@ export default {
   data() {
     return {
       // query: ''
+      // 이 query 문자열을 어디서 관리를 할 것인가를 생각해봐야 겠어요.
+      // 이 FormComponent는 실제 검색어 질의어를 나타내구요.
+      // 이 질이어는 직접 입력해서 검색어를 세팅할 수 있지만
+      // 추천 검색어나 최근 검색어에서 클릭하게 되면 클릭한 값을 입력값으로
+      // 설정해줘야 되요. 그 말은 이 query는 FormComponent가 관리하는 것 보다는
+      // FormComponent의 상위객체가 관리해주는게 좋습니다.
+      // 상위 객체는 app.js에서 정의한 뷰 인스턴스 겠죠.
+      // 즉 vue 인스턴스의 query값을 통해서 검색폼의 input값을 관리하는게 좋습니다.
+      // 그렇다라고 하면 FormComponent의 query값을 app.js의 query값으로 세팅하는
+      // 방법이 필요해요. 그렇게 하기위해서 index.html의 search-form directive를 사용할 때
+      // query값을 바인드 해줘야 되요.
+      // <search-form v-bind:value="query"></search-form>
+
       inputValue: this.value
     }
   },
--- 3-component/js/components/TabComponent.js
+++ 3-component/js/components/TabComponent.js
@@ -1,3 +1,21 @@
 export default {
-  template: '#tabs'
+  template: '#tabs',
+  // props: ['tabs'],
+  // data() {
+  //   return {
+  //     selectedTab: this.tabs[0]
+  //   }
+  // },
+  // methods: {
+  //   onClickTab(tab) {
+  //     this.selectedTab = tab
+  //     this.$emit('@change', tab)
+  //   }
+  // }
+  props: ['tabs', 'selectedTab'],
+  methods: {
+    onClickTab(tab) {
+      this.$emit('@change', tab)
+    }
+  }
 }
