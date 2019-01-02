ee7be50f04eb252405a56544028477ddcd52671d
smallbee <smallbee3@gmail.com>
2019-01-02 18:27:45 +0900

    component| FormComponent 1 - FormComponent.js added and imported in app.js

---
+++ 3-component/js/components/FormComponent.js
@@ -0,0 +1,20 @@
+export default {
+  template: '#search-form',
+  props: ['value'],
+  data() {
+    return {
+      query: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+
+    },
+    onKeyup() {
+
+    },
+    onReset() {
+
+    }
+  }
+}
