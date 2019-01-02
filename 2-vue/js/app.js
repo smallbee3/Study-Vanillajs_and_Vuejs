3ee0c55244f8546b5122f36adedc7aed8ae875af
smallbee <smallbee3@gmail.com>
2019-01-02 09:17:08 +0900

    vuejs| Keyword - (1) Show index number and keyword suggestion under the tab

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,4 +1,5 @@
 import SearchModel from './models/SearchModel.js'
+import KeywordModel from './models/KeywordModel.js'
 
 new Vue({
   el: '#app',
@@ -8,10 +9,12 @@ new Vue({
     tabs: ['추천 검색어', '최근 검색어'],
     // selectedTab: '추천 검색어', // -> 이렇게 하면 코드가 중복이니까요. Vue 인스턴스가 생성될 때 tabs 배열의 0번 인덱스로 설정하면 될 것 같아요
     selectedTab: '',
+    keywords: [],
     searchResult: []
   },
   created() {
     this.selectedTab = this.tabs[0]
+    this.fetchKeyword()
   },
   methods: {
     onSubmit(e) {
@@ -27,6 +30,11 @@ new Vue({
     },
     onClickTab(tab) {
       this.selectedTab = tab
+    },
+    fetchKeyword() {
+      KeywordModel.list().then(data => {
+        this.keywords = data
+      })
     },
     search() {
       SearchModel.list().then(data => {
